48a325448d111dd3cc0555fa126d657426bed293
senilm <senilmendapara.it20@scet.ac.in>
2023-12-03 17:13:52 +0530

    Handled MongoDB Errors

--- Backend/middleware/errorHandler.js
+++ Backend/middleware/errorHandler.js
@@ -3,11 +3,25 @@ import { StatusCodes } from "http-status-codes"
 
 const errorHandlerMiddleware = (err,req,res,next) =>{
     const customError = {
-        message:err.message || 'Something went wrong, Please try agin later...',
+        msg:err.message || 'Something went wrong, Please try agin later...',
         statusCode:err.statusCode || StatusCodes.INTERNAL_SERVER_ERROR
     }
 
-    return res.status(customError.statusCode).json({msg:customError.message})
+    if (err.code === 11000){
+        customError.msg = `Duplicate value entered for ${Object.keys(err.keyPattern)}`
+        customError.statusCode=StatusCodes.BAD_REQUEST
+    }
+    
+    if (err.name == 'ValidationError'){
+        customError.msg = `${Object.values(err.errors).map((item)=>item.message).join(' ,')}`
+        customError.statusCode = StatusCodes.BAD_REQUEST
+    }
+
+    if( err.name==="CastError"){
+        customError.statusCode = StatusCodes.BAD_REQUEST,
+        customError.msg=`Enter valid value ${err.value}`
+    }
+    return res.status(customError.statusCode).json({msg:customError.msg})
 }
 
 export default errorHandlerMiddleware
