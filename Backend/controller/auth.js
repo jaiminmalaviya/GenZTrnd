a77a66615ed4b159b3cff45b622d217edc90e9ed
senilm <senilmendapara.it20@scet.ac.in>
2023-12-03 16:24:26 +0530

    Implemented Authentication

--- Backend/controller/auth.js
+++ Backend/controller/auth.js
@@ -2,10 +2,40 @@ import User from "../models/user.js"
 import {StatusCodes} from "http-status-codes"
 import BadRequestError from "../errors/BadRequestError.js"
 import NotFoundError from "../errors/NotFoundError.js"
+import UnauthorizedError from "../errors/UnauthorizedError.js"
 
 export const login = async (req,res)=>{
-    res.status(StatusCodes.OK).json('hi from login')
+    const {email, password} =req.body
+    if (!email || !password){
+        throw new BadRequestError("Please provide all details")
+    }
+    const user = await User.findOne({email})
+
+    if(!user){
+        throw new NotFoundError("User not found")
+    }
+
+    const isVerified = await user.verifyPassword(password)
+    if(!isVerified){
+        throw new UnauthorizedError("Wrong password. Retry!")
+    }
+    
+    const accessToken = await user.genAccessToken()
+    const refreshToken = await user.genRefreshToken()
+
+    res.status(StatusCodes.OK).json({accessToken, refreshToken})
+
 }
+
+
 export const register = async (req,res)=>{
-    res.status(StatusCodes.OK).json(req.body)
+    const {name, email, password} = req.body
+
+    if(!name || !email || !password){
+        throw new BadRequestError("Please provide all details")
+    }
+
+    const user = await User.create(req.body)
+
+    res.status(StatusCodes.CREATED).json('User registered successfully')
 }
